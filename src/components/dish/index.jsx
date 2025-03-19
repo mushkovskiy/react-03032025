7eb82cf07fc25e8874126d76e773a16170c68846
mushkovskiy <eumu2022@gmail.com>
2025-03-19 20:53:42 +0300

    исправляет замечания к hw-4

--- src/components/dish/index.jsx
+++ src/components/dish/index.jsx
@@ -1,14 +1,10 @@
 import { useState } from "react";
 import { Counter } from "../counter/counter";
+import { useCount } from "../../hooks/use-count";
 
-const MAX_COUNT = 5;
-const MIN_COUNT = 0;
 export const Dish = ({ dish }) => {
   const { name } = dish;
-  const [count, setCount] = useState(0);
-
-  const onIncrement = () => setCount((prev) => (prev < 5 ? ++prev : MAX_COUNT));
-  const onDecrement = () => setCount((prev) => (prev > 0 ? --prev : MIN_COUNT));
+  const { count, onIncrement, onDecrement } = useCount();
 
   return (
     <div style={{ display: "flex", gap: "5px", flexDirection: "column" }}>

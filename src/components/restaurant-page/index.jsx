5f8106bd53c3583060bdee77ff2744966193bfc6
mushkovskiy <eumu2022@gmail.com>
2025-03-19 21:53:33 +0300

    добавляет hw-5

--- src/components/restaurant-page/index.jsx
+++ src/components/restaurant-page/index.jsx
@@ -1,25 +1,39 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { restaurants } from "../../mock";
 import { Restaurant } from "../restaurant";
 import { TabList } from "../tab-list";
+import { useScrollProgress } from "../../hooks/use-scroll-progress";
+import { ProgressBar } from "../progress-bar";
 
 const DEFAULT_ACTIVE_RESTAURANT_ID = restaurants[0].id;
 
 export const RestaurantPage = () => {
+  const ref = useRef(null);
   const [activeRestaurantId, setActiveRestaurantId] = useState(
     DEFAULT_ACTIVE_RESTAURANT_ID
   );
   const activeRestaurant = restaurants.find(
     ({ id }) => id === activeRestaurantId
   );
+  const { scrolledDistancePersentage } = useScrollProgress(ref);
+
   return (
-    <>
+    <div ref={ref}>
+      <ProgressBar progress={scrolledDistancePersentage} />
       <TabList
         items={restaurants}
         activeItemId={activeRestaurantId}
         setActiveItemId={setActiveRestaurantId}
       />
       <Restaurant restaurant={activeRestaurant} />
-    </>
+      <Restaurant restaurant={activeRestaurant} />
+      <Restaurant restaurant={activeRestaurant} />
+      <Restaurant restaurant={activeRestaurant} />
+      <Restaurant restaurant={activeRestaurant} />
+      <Restaurant restaurant={activeRestaurant} />
+      <Restaurant restaurant={activeRestaurant} />
+      <Restaurant restaurant={activeRestaurant} />
+      <Restaurant restaurant={activeRestaurant} />
+    </div>
   );
 };
